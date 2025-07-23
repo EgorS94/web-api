f994d2231e280b3cfa3d8e09d948ff10cba32a85
Егор Суртаев <CptRex501st@outlook.com>
2025-07-23 21:11:01 +0700

    Добавлена очистка элементов формы

--- client/src/layout/FormContact/FormContact.jsx
+++ client/src/layout/FormContact/FormContact.jsx
@@ -8,7 +8,13 @@ const FormContact = (props) => {
     const [contactAddress, setContactAddress] = useState("");
 
     const submit = () => {
+        if (contactName === `` || contactEmail === `` ||
+            contactPhone === `` || contactAddress === ``) return;
         props.addContact(contactName, contactEmail, contactPhone, contactAddress);
+        setContactName("");
+        setContactEmail("");
+        setContactPhone("");
+        setContactAddress("");
     }
 
     return (
@@ -18,24 +24,28 @@ const FormContact = (props) => {
                     <div className="mb-3">
                         <label className="form-label">Full name: </label>
                         <input className="form-control" type="text"
+                        value={contactName}
                             onChange={(e) => { setContactName(e.target.value) }}
                         />
                     </div>
                     <div className="mb-3">
                         <label className="form-label">E-mail: </label>
                         <input className="form-control" type="text"
+                        value={contactEmail}
                             onChange={(e) => { setContactEmail(e.target.value) }}
                         />
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Phone number: </label>
                         <input className="form-control" type="text"
+                        value={contactPhone}
                             onChange={(e) => { setContactPhone(e.target.value) }}
                         />
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Address: </label>
                         <textarea className="form-control" rows={2}
+                        value={contactAddress}
                             onChange={(e) => { setContactAddress(e.target.value) }}>
                         </textarea>
                     </div>
