8e22aaeae66d2e40ded4e22f5350d358e3716c87
Егор Суртаев <CptRex501st@outlook.com>
2025-07-23 16:47:57 +0700

    Подключение Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,56 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  Имя контакта
+                </th>
+                <th>
+                  Электронная почта
+                </th>
+                <th>
+                  Номер телефона
+                </th>
+                <th>
+                  Адресс
+                </th>
+                {/* <th>
+
+                </th> */}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Name</td>
+                <td>e-mail</td>
+                <td>phone number</td>
+                <td>address</td>
+              </tr>
+              <tr>
+                <td>Name 2</td>
+                <td>e-mail 2</td>
+                <td>phone number 2</td>
+                <td>address 2</td>
+              </tr>
+              <tr>
+                <td>Name 3</td>
+                <td>e-mail 3</td>
+                <td>phone number 3</td>
+                <td>address 3</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
