923bf22836d6123d1b6c60000ec4d9d9d2d2f5f9
Егор Суртаев <CptRex501st@outlook.com>
2025-08-11 17:23:16 +0700

    Bugfix & update

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,23 @@ import FormContact from "./layout/FormContact/FormContact";
 import TableContact from "./layout/TableContact/TableContact";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import ContactDetails from "./layout/ContactDetails/ContactDetails";
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 
 const App = () => {
 
-  const [contacts, setContacts] = useState(
-    [
-      // { id: 1, name: `Full name 1`, email: `ex1@mail.ru`, phone: `011-245-7036`, address: `東京都足立区東和２丁目１番４号ドーミー亀有301` },
-      // { id: 2, name: `Full name 2`, email: `ex2@mail.ru`, phone: `011-245-7034`, address: `東京都足立区東和２丁目１番４号ドーミー亀有302` },
-      // { id: 3, name: `Full name 3`, email: `ex3@mail.ru`, phone: `011-245-7039`, address: `東京都足立区東和２丁目１番４号ドーミー亀有303` }
-    ]
-  );
+  const [contacts, setContacts] = useState([]);
+  const location = useLocation();
 
-  const url = `${baseApiUrl}/contacts`;
 
   useEffect(() => {
+    const url = `${baseApiUrl}/contacts`;
     axios.get(url).then(
       res => setContacts(res.data)
     );
-  }, [])
+  }, [location.pathname])
 
   const addContact = (contactName, contactEmail, contactPhone, contactAddress) => {
 
@@ -55,7 +50,7 @@ const App = () => {
             </div>
           </div>
         } />
-        <Route path = "contacts/:id" element = {<ContactDetails />} />
+        <Route path="contacts/:id" element={<ContactDetails />} />
       </Routes>
     </div>
   );
