c05fdf72b57c9e6a6c09cb73d617032db349204d
Егор Суртаев <CptRex501st@outlook.com>
2025-08-11 20:20:39 +0700

    Страница добавления

---
+++ client/src/layout/FormContact/AppendContact.jsx
@@ -0,0 +1,37 @@
+import FormContact from "./FormContact";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AppendContact = () => {
+    
+    const navigate = useNavigate();
+    const baseApiUrl = process.env.REACT_APP_API_URL;
+
+    const addContact = (contactName, contactEmail, contactPhone, contactAddress) => {
+
+        let url = `${baseApiUrl}/contacts`;
+        const item = {
+            name: contactName,
+            email: contactEmail,
+            phone: contactPhone,
+            address: contactAddress
+        }
+        axios.post(url, item)
+            .then(
+                () => { navigate("/"); }
+            );
+    }
+
+    return (
+        <div className="card">
+            <div className="card-header">
+                <h1>Добавить контакт</h1>
+            </div>
+            <div className="card-body">
+                <FormContact addContact={addContact} />
+            </div>
+        </div>
+    );
+}
+
+export default AppendContact;
